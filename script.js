d1036ec2786f07ef5d5bd5f3afb43477f5e4f183
abhiborana <113044282+abhiborana@users.noreply.github.com>
2023-02-16 22:18:54 +0530

    Added new models infinix and others

--- script.js
+++ script.js
@@ -264,7 +264,88 @@ let lists = [
     ["vivo v9", "vivo y81", "vivo y81i", "vivo y83", "vivo y83 pro", "vivo y85", "vivo y89", "oppo a5", "oppo a3s"],
     ["oppo a77", "oppo a77s", "oppo a57", "oppo a53s", "oppo a31", "oppo a16k", "vivo y51", "vivo y51a", "vivo y31", "vivo y12s", "vivo y75", "vivo y33", "vivo y33t", "vivo y35", "vivo y16", "vivo y22", "vivo y33", "poco m2", "poco m3", "poco c3", "mi 10a", "mi 11e", "samsung a12", "samsung a04s", "nokia g21 ds"],
     ["samsung m20", "realme 3 pro", "realme 5 pro", "oppo f9", "oppo f9 pro"],
-    ["poco m3", "mi note 9 4g", "mi 9 power", "mi 9t", "oppo a9", "oppo a9x", "oppo f11", "vivo v3", "vivo y19", "vivo u20", "vivo y5s"]
+    ["poco m3", "mi note 9 4g", "mi 9 power", "mi 9t", "oppo a9", "oppo a9x", "oppo f11", "vivo v3", "vivo y19", "vivo u20", "vivo y5s"],
+    ["samsung a22 5g", "samsung f42 5g", "moto g50 5g"],
+
+    ["samsung a20", "samsung a30", "samsung a50", "samsung a30s", "samsung a50s", "samsung m30", "samsung m30s", "samsung m21", "samsung m31 prime", "samsung m21s", "samsung m31", "samsung f41", "samsung m21 2021", "samsung m10s"],
+
+    ["samsung a10", "samsung a10s", "samsung m10", "samsung m01s", "vivo vi y91", "vivo y93", "vivo y95", 'vivo y1s', "vivo y91i india", "vivo y91c 2020", "vivo y90", "vivo y93s", "oppo a5s", "oppo a7", "oppo a11k", "oppo a12", "oppo a12s", "oppo a12", "realme 3"],
+
+    ["samsung a31", "samsung a32 4g", "samsung a22 4g", "samsung f22", "samsung m32 4g", 'samsung m22', "samsung a33 5g"],
+
+    ["samsung a53 5g", "samsung a52", "samsung a52 5g", "samsung s20fe", "samsung s20fe 2022", "samsung s20 lite", "samsung a52s 5g", "samsung a51", "samsung m31s"],
+    [
+        "samsung m51", "samsung f62", "samsung m62", "samsung a71", "samsung m52 5g", "samsung m53 5g", "samsung a72", "samsung a73", "samsung note 10 lite", "samsung a81"],
+    [
+        "samsung a03", "samsung a03s", "samsung a13 4g", "samsung a13 5g", "samsung a23 4g", "samsung a23 5g", "samsung f23 5g", "samsung m13", "samsung m13 5g", "samsung m13 india", "samsung m23", "samsung m32 5g", "samsung m33 5g", "samsung a03core", "samsung a04", "samsung a04s", "vivo y33", "vivo y33s", "vivo y33t", "vivo y33e", "vivo y11s", "vivo y20s", "vivo y20a", "vivo y20t", "vivo y20 2021", "vivo y21", "vivo y21s", "vivo y21t 5g", "vivo y21g", "vivo y12a", "vivo y12g", "vivo y15s", "vivo y15a", "vivo y52 5g", "vivo y51a", "vivo y54s", "vivo y74s", "vivo y55 5g", "vivo y55s 5g", "vivo y75 5g ", "vivo y76 5g", "vivo y76s", "vivo y77", "vivo y3s 2021", "vivo y01", "vivo t1 5g india", "vivo t1x 4g", "vivo t1x 5g", "vivo t1x india", "vivo t2x", "oppo a11", "oppo a16", "oppo a16s", "oppo a16k", "oppo a16e", "oppo a54s", "oppo a55s 5g", "oppo a35", "oppo a8", "oppo a55 5g", "oppo a56 5g", "oppo a57 4g", "oppo a57 5g", "oppo a77", "narzo 50a", "narzo 50i", "narzo 50a prime", "narzo 50i prime", "narzo 50 5g", "realme c25y", "realme c21y", "realme c31", "realme c30", "realme c35", "mi 10 5g", "mi 10x 5g", "mi 10x pro 5g", "mi 10 prime+", "mi note 11e", "poco m4 5g", "poco m4 5g india", "tecno 6 go", "tecno spark 8", "tecno 8p", "tecno 8t", "tecno 8c", "infinix hot 10 lite", "infinix smart 5", "infinix 5 pro", "infinix hot 8", "infinix hot 11", "infinix hot 12i", "infinix hot 12 pro", "tecno spark 7", "tecno go", "tecno spark 9", "tecno spark 9t", "tecno 9t india", "tecno 9 pro"],
+
+    [
+        "oppo f11 pro", "reno 2z", "reno 2f", "realme x", "vivo v15", "vivo y30", "vivo y50", "vivo y50t", "vivo y70s", "vivo y70t", "vivo y51s 5g", "vivo z1 pro", "mi note 9t", "mi note 9", "mi note 9 5g", "mi 10x 4g"],
+    [
+        "realme 8 5g", "realme 7", "realme 6", "realme 6i", "oppo a74 5g", "oppo a52", "oppo a92", "oppo a72", "oppo a73 5g", "realme v13", "realme q2", "narzo 20 pro", "narzo 30", "narzo 30 pro", "oppo a53 5g", "oppo a54 5g", "oppo a93 5g", "oppo a93s 5g", "oppo k7x 5g", "oppo k9x 5g", "realme 8s 5g", "realme q3i 5g", "realme q3 5g", "realme v5 5g", "realme 6s", "realme 9 5g india", "samsung a11", "samsung m11"],
+    [
+        "realme 7i", "realme c17", "oppo a32", "oppo a33 2020", "oppo a53 2020", "oppo a54 4g", "oppo a55 4g", "oppo a53s 4g", "oppo a76", "oppo a96", "oppo k9s", "oppo k10", "oppo k10 5g china", "oppo k10x", "oppo a92s", "reno 4z 5g", "oppo a11s", "oppo a36", "realme 9 5g", "realme 8i", "realme 9i", "realme 9pro", "realme 9 se 5g", "realme 9 5g speed", "realme 6 pro", "realme x50 5g", "realme x3", "realme x3 super zoom", "realme q3s", "realme q5 5g", "realme v25", "narzo 50", "samsung f52 5g", "oneplus nord ce2 lite", "oneplus ace racing"],
+    ["oppo f9", "oppo f9 pro", "realme 2 pro", "realme 3 pro", "realme 5 pro", "realme u1", "samsung m20"],
+
+    ["realme 5", "realme 5i", "realme 6i global", "realme 5s", "realme c20", "realme c21", "realme c3", "realme c3i", "realme c11", "realme c12", "realme c15", "realme c25s", "realme c25", "realme c11 2021", "realme c30s", "realme c33", "realme 9i 5g", "realme v3", "realme v23", "realme v11 5g", "realme q2i", "narzo 30a", "narzo 10", "narzo 10a", "narzo 20", "narzo 20a", "oppo a15", "oppo a15s", "oppo a31", "oppo a9 2020", "oppo a5 2020", "oppo a53s 5g ", "oppo k10 5g", "oppo a17", "oppo a77s", "oppo a97", "oppo a57s", "oppo a57e", "vivo y53s", "vivo y22", "vivo y22s", "vivo y02s", "vivo y35", "vivo y77e", "vivo y77e", "vivo t1", "vivo y16", "vivo y75s", "vivo y73t", "vivo y52t", "vivo y51 2020", "vivo y52s", "vivo y12s", "vivo y31s", "vivo y31 2021", "vivo y20", "vivo y20i", "vivo y20g", "vivo y30 5g", "vivo y72", "vivo y72t 5g", "vivo y32", "vivo y21a", "vivo y21e", "iqoo z6", "iqoo z6 lite 5g", "iqoo z3 5g", "iqoo u3 5g", "iqoo u5 5g", "iqoo u5x", "samsung m02s", "samsung f02s", "samsung m12", "samsung a02", "samsung m02", "samsung a32 5g", "samsung a12", "samsung a02s", "samsung f12", "samsung f13", "mi 11 prime", "mi 11 prime 5g", "mi a1 2022", "mi note 11r", "poco m5", "oneplus nord n20 se", "wiko t10"],
+
+    ["oppo f17 pro", "oppo a93", "reno 4", "reno 4 5g", "oppo f19", "oppo f19 pro", "oppo f19 pro +", "oppo f19s", "oppo f21 pro 4g", "oppo f21 pro 5g", "oppo a74 4g", "oppo a94", "oppo a94 5g", "reno 6z", "oppo a95", "oppo a95 5g", "reno 2", "reno 4 lite", "reno 5 lite", "reno 6 lite", "reno 7 lite", "reno 8 lite", "reno 5z", "reno  5f", "reno 4se", "reno 4f", "oppo find x5 lite 5g", "reno 7", "reno 7 5g", "reno 7 se 5g", "reno 7z 5g", "reno 8 4g", "reno 8 5g", "reno 8z", "oppo a96 5g china", "realme 9 4g", "realme 9 pro +", "realme 7 pro", "realme x7", "realme 8", "realme 8pro", "realme q2 pro", "realme v15 5g", "narzo 50 pro 5g", "oneplus nord ce2 5g"],
+
+    ["reno 8 pro 5g", "reno 8 pro + 5g", "honor x8"],
+
+    ["oneplus nord", "reno 6", "reno 5", "reno 5 5g", "reno 7 5g china", "reno 5k", "lg k9 5g", "oppo find x3 lite", "realme gt 5g", "realme gt master", "realme gt neo", "realme gt neo 2t", "realme q3 pro 5g", "realme q3 pro carnival", "realme x7 max 5g", "oneplus ce 5g", "oneplus nord 2 5g", "oneplus nord", "oneplus nord 2t"],
+
+    [
+        "mi 9 prime", "mi 9", "mi 9t", "mi 9 power", "mi 9 prime", "mi note 9 4g", "poco m2", "poco m3", "vivo y19", "vivo u3", "vivo u20", "vivo y5s"],
+
+    ["mi 9a", "mi 9c", "mi 9i", "mi 9 india", "mi 9 activ", "mi 9a sport", "mi 9i sport", "mi 9 at", "poco c3", "poco c31", "mi 10a"],
+
+    ["mi 8", "mi 8a", "mi 8a dual", "mi note 8"],
+
+    ["mi note 11 5g china", "mi note 10 pro 5g china", "poco x3 gt", "poco m4 pro 5g", "mi note 11t 5g", "mi note 11s 5g", "realme gt neo2", "realme gt 2", "realme gt neo 3t", "realme q5 pro 5g", "oppo k10 pro 5g", "reno 8 pro china", "oneplus 9r t", "moto g22", "tecno camon 18 prime"],
+
+    ["mi note 10 4g", "mi note 10s", "mi note 11 global", "mi note 11s 4g",
+        "poco m4 pro 4g", "mi note 11 se india", "poco m5s", "mi note 10 5g", "poco m3 pro", "mi note 10t 5g", "mi 10", " mi 10 2022", "mi 10 prime 2022", "mi note 11 4g", "mi note 11 se"],
+
+    ["mi note 9 pro", "mi note 9s", "mi note 9 pro max", "mi note 10 pro", "mi note 10 pro max", "mi note 11 pro", "mi note 11 pro +", "mi note 11e pro", "poco f3", "poco f4", "poco f2 pro", "mi 11x", "mi 11x pro", "mi 11i", "mi 11t", "mi 11t pro", "poco x4 pro 5g", "mi k40", "mi k40 pro", "mi k40 pro +", "mi k40s", "poco x2", "mi k30", "mi k30s", "mi k30i", "mi k30 pro", "mi k30 ultra", "poco x3", "poco x3 pro", "poco m2 pro", "mi 10i", "mi 10t 5g", "mi 10t pro 5g", "mi 10t lite", "mi black shark 4", "mi black shark 4 pro", "mi 4s", "mi 4s pro", "samsung a80", "samsung a90", "lg k92", "honor x10", "huawei p smart 2021", "vivo t1 5g china", "iqoo z5", "iqoo neo5 se"],
+
+    ["mi note 8 pro", "oppo a9", "oppo f11"]
+    ,
+    ["mi note 7", "mi note 7 pro", "mi note 7s"],
+
+    ["samsung s10 lite", "samsung a91", "samsung note 20", "samsung a71 5g", "infinix note 12 vip", "infinix x672"],
+
+    ["poco f3 gt", "poco f4 gt", "mi k40 gaming", "mi k50 gaming"],
+
+    ["mi 7", "mi 7 pro", "mi y3"],
+
+    ["vivo x60", "vivo x70", "iqoo 8 5g", "iqoo 9 5g"],
+
+    ["oneplus 7", "oneplus 6t", "vivo v11", "vivo v11 pro", "vivo x21s", "mi 9", "mi 9 lite", "mi 9 pro", "realme xt"]
+    ,
+    ["oneplus 8t", "oneplus 9", "oneplus 9 r", "mi 11 lite", "mi 11 lite 5g ne",
+        "mi 12 lite", "reno 7 pro 5g", "realme x7 pro 5g"],
+
+    ["oneplus 10t", "oneplus 10r", "oneplus ace 5g", "oneplus ace pro", "realme gt neo 3", "realme gt 2 pro", "iqoo 9se", "iqoo 7 5g", "iqoo neo 6", "iqoo neo 6 se", "iqoo neo 5s 5g", "iqoo neo 5 5g", "vivo s15", "vivo t2 5g"],
+
+    ["vivo v17", "vivo v17 pro", "vivo v19", "vivo x30 5g", "vivo x30 pro 5g", "iqoo 3 5g"],
+    ["vivo v15 pro", "vivo x27", "mi 9t", "mi k20", "mi k20 pro"],
+
+    ["vivo y12", "vivo y15", "vivo y17", "vivo y11 2019", "vivo u10", "vivo y12 i", "vivo y3", "vivo y3s"],
+
+    ["vivo s1", "vivo s1 pro", "vivo z1x", "vivo y9s", "vivo z5", "iqoo neo"],
+
+    ["vivo v9", "vivo y83", "vivo y83 pro", "vivo y81", "vivo y85", "vivo z1", "vivo z3x", "vivo y89", "vivo v9 youth", "oppo a3s", "oppo a5", "oppo f7", "realme 2", "realme c1", "oneplus 6"],
+
+    ["vivo y73", "vivo y73s", "vivo y75 4g", "vivo v20", "vivo v20 se", "vivo v21", "vivo v21 5g", "vivo v21 e", "vivo v23 e", "vivo v23 e 5g", "vivo v25 5g", "vivo v25 e", "vivo y70", "vivo y71 t", "vivo s10 e", "vivo s15 e 5g", "vivo s9e 5g", "vivo x80 lite", "vivo t1 pro 5g", "vivo t1 5g malaysia", "vivo t1 44w", "iqoo z6 pro 5g", "oppo f15", "oppo f17", "oppo a73", "oppo a91", "reno 3"],
+
+    ["vivo v20 pro", "vivo s7", "vivo s10", "vivo s10 pro", "vivo v23 5g"],
+
+    ["infinix hot 10", "infinix hot 12", "infinix hot 12 play", "infinix zero 8", "infinix x683", "infinix note 8i", "tecno spark 6", "tecno camon 16", "tecno camon 16 pro"],
+
+    ["infinix hot 9 play", "infinix hot 10 play", "infinix hot 10s", "infinix hot 10s nfc", "infinix hot 10t", "infinix hot 11 play", "infinix note 11", "infinix note 12", "infinix note 12 turbo", "infinix note 12 pro"],
+
+    ["infinix note 11 pro", "infinix note 11s", "infinix x697", "tecno pova 2", "tecno pova 3", 'tecno pova 5g', "infinix note 7"]
 ];
 
 // ONLY TRIAL CODE HERE
@@ -376,7 +457,6 @@ submitContribute.addEventListener("click", () => {
             reply_to: "abhiborana.a@gmail.com",
         }).then(() => {
             alert("Sent Successfully")
-            location.reload()
             nameContribute = ""
             modelOne = ""
             modelTwo = ""
